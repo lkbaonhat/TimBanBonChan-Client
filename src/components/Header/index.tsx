727b3d98dc8bb7fab48623bcc368a148ec7d02d7
Lê Kim Bảo Nhật <lekimbaonhat.92.2017@gmail.com>
2025-04-12 19:03:01 +0700

    [UPDATE] Responsive sign in, sign up page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import HeaderAuth from "./HeaderAuth/HeaderAuth";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import ROUTES from "@/constants/routes";
 
-export default function Header() {
-  return <div>index</div>;
+type NavigationItem = {
+  label: string;
+  href: string;
+};
+
+type HeaderProps = {
+  items?: NavigationItem[];
+};
+
+function Header({ items }: HeaderProps) {
+  return (
+    <header className={styles.header}>
+      {/* Logo Section */}
+      <div className={styles.logo}>
+        <Link className={styles.itemsLink} to={ROUTES.PUBLIC.HOME}>
+          <img src={} alt="TimBanBonChan_Logo" />
+        </Link>
+      </div>
+
+      {/* Navigation Section */}
+      <nav className={styles.navigation}>
+        {items?.map((item) => (
+          <Link key={item.label} to={item.href}>
+            {item.label}
+          </Link>
+        ))}
+      </nav>
+
+      {/* User Section */}
+      {/* <div className={styles.userSection}>
+        <HeaderAuth user={user} onLogout={handleLogout} />
+      </div> */}
+    </header>
+  );
 }
+
+export default Header;
